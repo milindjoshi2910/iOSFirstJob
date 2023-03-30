052a57bce93ea33cfb19f5de72527a97adc8d3d5
mymilind <milind.joshi@mindstix.com>
2023-03-30 16:07:31 +0530

    First Commit

---
+++ iOSSampleApp/iOSSampleApp/iOSSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSampleAppApp.swift
+//  iOSSampleApp
+//
+//  Created by Milind Joshi on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
