c47560a67affd48af6efef6d56f4718d333dac18
mymilind <milind.joshi@mindstix.com>
2023-03-30 16:31:56 +0530

    Third Commit.

--- iOSSampleApp/iOSSampleApp/ContentView.swift
+++ iOSSampleApp/iOSSampleApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world first!")
+            Text("Hello, world first Job!")
         }
         .padding()
     }
